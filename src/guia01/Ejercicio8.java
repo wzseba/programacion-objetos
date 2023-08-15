d727bd2df42ec80c245bf7761cc29f253eeaa2fc
wzseba <wzseba@gmail.com>
2023-08-14 22:02:26 -0300

    se agrego variable

--- src/guia01/Ejercicio8.java
+++ src/guia01/Ejercicio8.java
@@ -5,6 +5,7 @@ public class Ejercicio8 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mi mundo");
+		String nombre = "sebastian git";
 	}
 
 }
