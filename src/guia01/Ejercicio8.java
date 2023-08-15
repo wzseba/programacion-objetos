a0853fc7ce7a729bb0ad9b62584beae464791291
wzseba <wzseba@gmail.com>
2023-08-14 23:43:26 -0300

    borrando cosas

--- src/guia01/Ejercicio8.java
+++ src/guia01/Ejercicio8.java
@@ -4,7 +4,7 @@ public class Ejercicio8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola mi mundo");
+		
 		String nombre = "sebastian git";
 		String altura = "12";
 	}
